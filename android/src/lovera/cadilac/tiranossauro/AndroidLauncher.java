5277fe986a604ce29aa2434e3984d359e725f6d3
Santiago Lovera <santiago.lovera@gmail.com>
2016-10-28 18:53:33 -0200

    Initial commit

---
+++ android/src/lovera/cadilac/tiranossauro/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package lovera.cadilac.tiranossauro;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import lovera.cadilac.tiranossauro.GameClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameClass(), config);
+	}
+}
