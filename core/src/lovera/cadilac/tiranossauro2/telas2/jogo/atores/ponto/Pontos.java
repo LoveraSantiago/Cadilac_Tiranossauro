0d0e32e050fcaf0e1c435c00bc11d07d00cf00c4
Santiago Lovera <santiago.lovera@gmail.com>
2016-12-10 08:12:57 -0200

    Trocando implementacao de pontos

---
+++ core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/ponto/Pontos.java
@@ -0,0 +1,42 @@
+package lovera.cadilac.tiranossauro2.telas2.jogo.atores.ponto;
+
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Pontos {
+
+    private final Vector2 pontoTemp;
+
+    private final List<Float> listaX;
+    private final List<Float> listaY;
+
+    public Pontos() {
+        this.listaX = new ArrayList<Float>();
+        this.listaY = new ArrayList<Float>();
+
+        this.pontoTemp = new Vector2();
+    }
+
+    public void addPontos(float x, float y){
+        this.listaX.add(x);
+        this.listaY.add(y);
+    }
+
+    public Vector2 consumirProximoPonto(){
+        this.pontoTemp.set(this.listaX.get(0), this.listaY.get(0));
+        limparPrimeiraPosicao();
+        return this.pontoTemp;
+    }
+
+    private void limparPrimeiraPosicao(){
+        this.listaX.remove(0);
+        this.listaY.remove(0);
+    }
+
+    public void limparPontos(){
+        this.listaY.clear();
+        this.listaX.clear();
+    }
+}
--- core/src/lovera/cadilac/tiranossauro2/telas2/jogo/controladores/PontoManager.java
+++ core/src/lovera/cadilac/tiranossauro2/telas2/jogo/controladores/PontoManager.java
@@ -1,17 +1,16 @@
 package lovera.cadilac.tiranossauro2.telas2.jogo.controladores;
 
 import lovera.cadilac.tiranossauro2.contratos.tipo.TipoSingleton;
-import lovera.cadilac.tiranossauro2.telas2.jogo.atores.ponto.Ponto;
-import lovera.cadilac.tiranossauro2.telas2.jogo.atores.ponto.PoolDePontos;
+import lovera.cadilac.tiranossauro2.telas2.jogo.atores.ponto.Pontos;
 
 public class PontoManager implements TipoSingleton{
 
     private static PontoManager pontoManager;
 
-    private final PoolDePontos poolDePontos;
+    private final Pontos pontos;
 
     public PontoManager() {
-        this.poolDePontos = new PoolDePontos();
+        this.pontos = new Pontos();
     }
 
     @Override
@@ -19,11 +18,11 @@ public class PontoManager implements TipoSingleton{
         pontoManager = this;
     }
 
-    public Ponto criarPonto(float x, float y){
-        return this.poolDePontos.obtain().init(x, y);
+    public static PontoManager getInstancia() {
+        return pontoManager;
     }
 
-    public void destruirPonto(Ponto ponto){
-        this.poolDePontos.free(ponto);
+    public Pontos getPontos() {
+        return pontos;
     }
 }
