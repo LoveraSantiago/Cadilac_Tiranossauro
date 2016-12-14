467fa2fee8937a364a478ae40d1d713964a39b48
Santiago Lovera <santiago.lovera@gmail.com>
2016-12-14 06:50:45 -0200

    fixes

--- core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/entidades/quadrantes/ClassificadorDeQuadrante.java
+++ core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/entidades/quadrantes/ClassificadorDeQuadrante.java
@@ -34,6 +34,7 @@ public final class ClassificadorDeQuadrante {
             this.quadranteAtual = listaDeQuadrantes.get(this.contador);
             this.quadranteAtual.setPontos(ptOrigem, pt.x, pt.y);
             if(this.quadranteAtual.meuQuadrante()){
+                System.out.println("Quadrante n:" + this.contador);
                 break;
             }
         }
