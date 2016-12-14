4db242f008e1b0acba8224085dc3c68ed979bcc1
Santiago Lovera <santiago.lovera@gmail.com>
2016-12-14 06:05:39 -0200

    implementado calculador de velocidade

---
+++ core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/corredor/CalculadorVelocidade.java
@@ -0,0 +1,30 @@
+package lovera.cadilac.tiranossauro2.telas2.jogo.atores.corredor;
+
+import com.badlogic.gdx.math.Vector2;
+
+class CalculadorVelocidade {
+
+    private final float VELOCIDADE = 6f;
+
+    private float tempoTotal;
+    private float tempoPonto;
+
+    private final Vector2 resultVelocidade;
+
+    public CalculadorVelocidade() {
+        this.resultVelocidade = new Vector2();
+    }
+
+    public void calcularVelocidadePercurso(int pontos, float percursoTotal){
+        this.tempoTotal = percursoTotal / VELOCIDADE;
+        this.tempoPonto = this.tempoTotal / pontos;
+    }
+
+    public Vector2 calcularVelocidadePonto(Vector2 posicaoAtual, Vector2 ponto){
+        this.resultVelocidade.x = (posicaoAtual.x - ponto.x) / this.tempoPonto;
+        this.resultVelocidade.y = (posicaoAtual.y - ponto.y) / this.tempoPonto;
+
+        return this.resultVelocidade;
+    }
+
+}
