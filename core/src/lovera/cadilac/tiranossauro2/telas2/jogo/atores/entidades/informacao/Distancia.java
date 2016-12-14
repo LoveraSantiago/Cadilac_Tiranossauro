54597a05bf56618590aa8533ea1077f572e6961a
Santiago Lovera <santiago.lovera@gmail.com>
2016-12-13 22:24:56 -0200

    limpeza

--- core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/entidades/informacao/Distancia.java
+++ core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/entidades/informacao/Distancia.java
@@ -2,7 +2,7 @@ package lovera.cadilac.tiranossauro2.telas2.jogo.atores.entidades.informacao;
 
 public final class Distancia {
 
-    private Float espacoPercorrido;
+    private float espacoPercorrido;
 
     public void addEspacoPercorrido(float pt1X, float pt1Y, float pt2X, float pt2Y){
         this.espacoPercorrido += (float) (Math.sqrt(Math.pow(pt2X - pt1X, 2) + Math.pow(pt2Y - pt1Y, 2)));
