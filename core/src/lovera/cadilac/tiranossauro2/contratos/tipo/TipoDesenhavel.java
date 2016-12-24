d3407137a9708fdbd8810cd3b60dac5830126cad
Santiago Lovera <santiago.lovera@gmail.com>
2016-12-24 09:52:20 -0200

    lembrete

--- core/src/lovera/cadilac/tiranossauro2/contratos/tipo/TipoDesenhavel.java
+++ core/src/lovera/cadilac/tiranossauro2/contratos/tipo/TipoDesenhavel.java
@@ -2,5 +2,6 @@ package lovera.cadilac.tiranossauro2.contratos.tipo;
 
 public interface TipoDesenhavel {
 
+    //Todo ver o quanto o objeto passado como parametro é necessario
     void meDesenhar(Object objeto);
 }
