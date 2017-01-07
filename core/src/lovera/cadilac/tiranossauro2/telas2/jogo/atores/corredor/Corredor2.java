c296ab7d14d4943735a4f12ee6279e0ab72a4ffd
Santiago Lovera <santiago.lovera@gmail.com>
2017-01-07 10:04:37 -0200

    limpeza mais substituicao de classe

--- core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/corredor/Corredor2.java
+++ core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/corredor/Corredor2.java
@@ -43,32 +43,6 @@ public final class Corredor2 implements TipoParseavel, TipoDesenhavel, MsgFromMo
         }
     }
 
-//    @Override
-//    public void meDesenhar(Object objeto) {
-//        if(this.faseManager2.isFaseAtual(Fase2.JOGANDO)){
-//           meDesenhar_FaseJogando();
-//        }
-//        else if(this.faseManager2.isFaseAtual(Fase2.ACEITAR_ENTRADA)){
-//            meDesenhar_FaseAceitarEntrada();
-//        }
-//        else if(this.faseManager2.isFaseAtual(Fase2.ACAO)){
-//            meDesenhar_FaseAcao();
-//        }
-//        else{
-//            meDesenhar_FaseOutras();
-//        }
-//    }
-
-    private void meDesenhar_FaseJogando(){
-//        this.calcAngulo.rotacionarIda();
-        this.lataria.meDesenhar(null);
-    }
-
-    private void meDesenhar_FaseAceitarEntrada(){
-//        this.calcAngulo.rotacionarVolta();
-        this.lataria.meDesenhar(null);
-    }
-
     private void meDesenhar_FaseOutras(){
         this.calcAngulo.rotacionarParado();
         this.lataria.meDesenhar(null);
