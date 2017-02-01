6391ed0b66c9208126b704bff51167691364c53f
Santiago Lovera <santiago.lovera@gmail.com>
2017-01-31 22:48:06 -0200

    incrementando informacoes

--- core/src/lovera/cadilac/tiranossauro2/utils/custom_anotacoes/Revisao.java
+++ core/src/lovera/cadilac/tiranossauro2/utils/custom_anotacoes/Revisao.java
@@ -5,4 +5,11 @@ public @interface Revisao {
     String dataModificacao();
     String dataRevisao();
 
+    boolean classePublica();
+    String justificativa_classePublica();//Justificar casos positivos
+
+    boolean classeFinal();
+    String justificativa_classeFinal();//Justificar casos negativos
+
+    String[] todos();
 }
