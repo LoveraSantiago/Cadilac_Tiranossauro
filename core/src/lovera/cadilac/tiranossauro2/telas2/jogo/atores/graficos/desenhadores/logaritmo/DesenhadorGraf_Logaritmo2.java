6773ba16ae0b30bd2d201c95e2bb4fdb06e340de
Santiago Lovera <santiago.lovera@gmail.com>
2017-02-03 23:01:00 -0200

    limpeza

--- core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/graficos/desenhadores/logaritmo/DesenhadorGraf_Logaritmo2.java
+++ core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/graficos/desenhadores/logaritmo/DesenhadorGraf_Logaritmo2.java
@@ -4,7 +4,6 @@ import lovera.cadilac.tiranossauro2.telas2.jogo.atores.entidades.equacoes.Equaca
 import lovera.cadilac.tiranossauro2.telas2.jogo.atores.graficos.desenhadores.DesenhadorGraf_LOGEXP;
 
 //LINK DE AJUDA: https://www.desmos.com/calculator/auubsajefh
-//TODO: refatorar DesenhadorGraficoGraf_Exponencial com DesenhadorGraficoGraf_Logaritmo classes muito iguais REALIZAR ESSA REFATORACAO SOMENTE APOS AJUSTAR OS GRAFICOS PARA MAIORES ABERTURAS ESTAO MUITO VERTICAIS AMBOS
 public final class DesenhadorGraf_Logaritmo2 extends DesenhadorGraf_LOGEXP {
 
     public DesenhadorGraf_Logaritmo2() {
