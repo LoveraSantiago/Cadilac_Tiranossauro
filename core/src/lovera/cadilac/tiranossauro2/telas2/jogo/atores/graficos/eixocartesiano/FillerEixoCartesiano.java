e604030dc0f083d1b19b4fdbaa274e675a76e6d5
Santiago Lovera <santiago.lovera@gmail.com>
2017-02-09 22:45:22 -0200

    implementando filler de cartesianos

---
+++ core/src/lovera/cadilac/tiranossauro2/telas2/jogo/atores/graficos/eixocartesiano/FillerEixoCartesiano.java
@@ -0,0 +1,27 @@
+package lovera.cadilac.tiranossauro2.telas2.jogo.atores.graficos.eixocartesiano;
+
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+
+import lovera.cadilac.tiranossauro2.contratos.tipo.TipoDesenhavel;
+import lovera.cadilac.tiranossauro2.telas2.gerais.NinePatchLeitor;
+import lovera.cadilac.tiranossauro2.telas2.jogo.controladores.unicos.NinePatchUnico;
+
+import static lovera.cadilac.tiranossauro2.utils.imagens.Img9Patch.FILLER_HORIZONTAL;
+import static lovera.cadilac.tiranossauro2.utils.imagens.Img9Patch.FILLER_VERTICAL;
+
+public final class FillerEixoCartesiano implements TipoDesenhavel{
+
+    private final NinePatch fillerH;
+    private final NinePatch fillerV;
+
+    public FillerEixoCartesiano() {
+        NinePatchLeitor ninePatchLeitor = NinePatchUnico.getInstancia().getNinePatchLeitor();
+        this.fillerH = ninePatchLeitor.getNinePatch(FILLER_HORIZONTAL);
+        this.fillerV = ninePatchLeitor.getNinePatch(FILLER_VERTICAL);
+    }
+
+    @Override
+    public void meDesenhar(Object objeto) {
+
+    }
+}
--- core/src/lovera/cadilac/tiranossauro2/utils/imagens/Img9Patch.java
+++ core/src/lovera/cadilac/tiranossauro2/utils/imagens/Img9Patch.java
@@ -4,7 +4,9 @@ public enum Img9Patch {
 
     AREA("area_jogavel2"),
     EIXO_X("seta_x_eixocart"),
-    EIXO_Y("seta_y_eixocart");
+    EIXO_Y("seta_y_eixocart"),
+    FILLER_HORIZONTAL("filler_horizontal"),
+    FILLER_VERTICAL("filler_vertical");
 
     private String nomeImg;
 
